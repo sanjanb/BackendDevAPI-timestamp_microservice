20fe0153795658a8cc20ac2a80e20e3033f95a21
Sanjan B M <acharyasanjan931@gmail.com>
2024-12-20 08:00:42 +0000

    Everything

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/date", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
